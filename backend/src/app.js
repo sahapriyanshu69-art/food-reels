6a1c067cacda5511642ca0adc0fb239b455cfac0
Priyanshu Saha <sahapriyanshu69@gmail.com>
2026-05-28 22:45:17 +0530

    Fix production CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,28 +1,26 @@
 const express = require('express');
-const cookieParser=require('cookie-parser')
-const authRoutes=require("./routes/auth.routes.js")
-const foodRoutes=require('./routes/food.routes.js');
-const foodPartnerRoutes=require('./routes/foodpartner.routes.js');
+const cookieParser = require('cookie-parser');
+const authRoutes = require("./routes/auth.routes.js");
+const foodRoutes = require('./routes/food.routes.js');
+const foodPartnerRoutes = require('./routes/foodpartner.routes.js');
 const cors = require('cors');
 
+const app = express();
+
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "https://food-reels-livid.vercel.app"
-    ],
+    origin: "https://food-reels-livid.vercel.app",
     credentials: true
 }));
 
 app.use(cookieParser());
 app.use(express.json());
 
-
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello world");
-})
-app.use('/api/auth',authRoutes);
-app.use('/api/food',foodRoutes);
-app.use('/api/foodpartner',foodPartnerRoutes);
+});
+
+app.use('/api/auth', authRoutes);
+app.use('/api/food', foodRoutes);
+app.use('/api/foodpartner', foodPartnerRoutes);
 
 module.exports = app;
-
